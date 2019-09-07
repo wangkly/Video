cc7a080d906c16c3d5fc68ae0ccc0d681fac52e9
wangkly <wangkly123@gmail.com>
2019-09-07 11:26:07 +0800

    modify

--- app/src/main/java/com/wangky/video/MainActivity.java
+++ app/src/main/java/com/wangky/video/MainActivity.java
@@ -1,14 +1,26 @@
 package com.wangky.video;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.wangky.video.activities.PlayActivity;
+import com.wangky.video.adapter.VideoListAdapter;
+import com.wangky.video.beans.LocalVideoItem;
+import com.wangky.video.util.Utils;
+
+import java.util.List;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -17,25 +29,14 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.wangky.video.activities.AvgleActivity;
-import com.wangky.video.activities.PlayActivity;
-import com.wangky.video.activities.VideosActivity;
-import com.wangky.video.adapter.VideoListAdapter;
-import com.wangky.video.beans.LocalVideoItem;
-import com.wangky.video.util.Utils;
-
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
 
     private static final  int PERMISSION_REQUESTCODE = 1;
 
+    private final int OPEN_FILE_MANAGER = 2;
 
     private RecyclerView mRecyclerView;
-
 
     private VideoListAdapter adapter;
 
@@ -45,30 +46,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             RecyclerView.ViewHolder holder = (RecyclerView.ViewHolder) v.getTag();
-
-
             int position =  holder.getAdapterPosition();
 
-
             LocalVideoItem item = adapter.getItemAtPosition(position);
-
             Boolean orientation = item.getWidth() > item.getHeight() ? true : false;
-
             String data = item.getData();
             String title = item.getTitle();
 
             Intent intent = new Intent(MainActivity.this, PlayActivity.class);
-
             intent.putExtra("orientation",orientation);
-
             intent.putExtra("data",data);
-
             intent.putExtra("title",title);
-
-
             startActivity(intent);
-
-
         }
     };
 
@@ -88,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,18 +86,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         mRecyclerView = findViewById(R.id.video_list);
-
 
         if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
-
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.INTERNET,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUESTCODE);
         }else {
-
-
             remainOperation();
         }
 
@@ -149,18 +132,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            Intent intent = new Intent(MainActivity.this,VideosActivity.class);
-
-            startActivity(intent);
+        if (id == R.id.action_open) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(intent,OPEN_FILE_MANAGER);
 
             return true;
         }
 
         if(id == R.id.favor){
-            Intent intent = new Intent(MainActivity.this,AvgleActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(MainActivity.this,AvgleActivity.class);
+//            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -171,24 +154,39 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
         switch (requestCode){
-
             case PERMISSION_REQUESTCODE:
-
                 if(PackageManager.PERMISSION_GRANTED == grantResults[0]){
-
                     remainOperation();
-
                 }
 
                 break;
-
-
             default:
 
 
                 break;
-
         }
-
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            if(resultCode == Activity.RESULT_OK){
+                switch (requestCode){
+                    case OPEN_FILE_MANAGER:
+                        Uri uri = data.getData();
+                        System.out.println(uri.getPath());
+
+                        break;
+
+
+                     default:
+
+                         break;
+                }
+            }
+    }
+
+
+
+
 }
